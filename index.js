8a6516377949d8e57ff606774f063899e02ca5df
daru23 <daru015@gmail.com>
2015-08-30 23:59:18 +0200

    simple logic

--- index.js
+++ index.js
@@ -12,7 +12,8 @@
 /* Required packages */
 var redis = require('redis'),
 /* Required private modules */
-    config = ('./config.json');
+    config = require('./config.json'),
+    msgModule = require('./messages.js');
 
 /*Master Logic*/
 var listenClient  = redis.createClient(config.redis.port, config.redis.server),
@@ -29,7 +30,21 @@ listenClient.on('error', function (error) {
 // Catch redis message
 listenClient.on("message", function (channel, message) {
 
-    console.log('Master Broker has a message : %s', message);
+    var msg;
+    try{
+        msg = JSON.parse(message);
+        msg = msgModule.verify(msg); // validation of the message
+
+        //Asking for a channel
+        if (msg.respondChannel === ''){
+            //assign channel
+        //Sending a message
+        } else {
+            // look for the service and forward the message
+        }
+    }catch(e){
+        console.log("Message is not a valid json")
+    }
 
 });
 
